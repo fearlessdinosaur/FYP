5815816772935fd1281352079fd5328054e4a85a
fearlessdinosaur <c15436172@mydit.ie>
2018-11-28 14:29:55 +0000

    created basic echo server

--- Client.py
+++ Client.py
@@ -1,29 +1,31 @@
 import asyncio
+import readchar
+from tkinter import *
 class client:
     
     def __init__(self):
+        self.Message_Buffer = []
         self.loop = asyncio.get_event_loop()
-        self.loop.run_until_complete(client.setup(self.loop,))
+        self.loop.run_until_complete(asyncio.gather(client.send_message(self.loop,self,),client.get_message(self.loop,self,)))
+        
+    async def send_message(loop,self):
+        reader, writer = await asyncio.open_connection('127.0.0.1', 1661,
+                                                   loop=loop)
+        while 1:
+            message = input(">>")
+            print('Send: %r' % message)
+            writer.write(message.encode())
 
-    async def setup(loop):
-        print("connection established")
-        reader,writer = await asyncio.open_connection('127.0.0.1',8888,loop=loop)
-        await client.read(reader)
+    async def get_message(loop,self):
+        reader, writer = await asyncio.open_connection('127.0.0.1', 1661,
+                                                   loop=loop)
 
-
-    async def read(reader):
-        print("reading")
-        data = await reader.read(100)
-        print(data)
-        await client.write(writer)
-
-    async def write(writer):
-        print("writing")
-        data=input("enter message")
-        writer.write(data.encode())
-        await client.read(reader)
-
-
+        while True:
+            data = await reader.read(100)
+            print('Received: %r' % data.decode())
+            if(data.decode() == "{end}"):
+                break;
+        writer.close()
 client()
 
             
--- Server.py
+++ Server.py
@@ -1,18 +1,22 @@
 import asyncio
-
+clients = []
 class Server:
     
-    async def uplink(reader,writer):
+    async def uplink(reader,writer,):
         print("uplink established")
-        data = await reader.read(100)
-        msg = data.decode()
-        print(msg)
-
-        writer.write(data)
+        clients.append(writer)
+        print(clients)
+        while True:
+            data = await reader.read(100)
+            msg = data.decode()
+            print(msg)
+            for client in clients:
+                print(client.get_extra_info('peername'))
+                client.write(data)
         
     def __init__(self):
         self.loop = asyncio.get_event_loop()
-        self.serv = asyncio.start_server(Server.uplink,'127.0.0.1',8888,loop=self.loop)
+        self.serv = asyncio.start_server(Server.uplink,'127.0.0.1',1661,loop=self.loop)
         self.server = self.loop.run_until_complete(self.serv)
         self.loop.run_forever()
         
